6ab948d0b02fb17441434542f9a5b8ab58c05e62
huappe <155170805+huappe@users.noreply.github.com>
2022-05-28 12:59:41 +0000

    Update library.properties

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,4 +3,17 @@
 CURRENT_DIR=$(dirname "$0")
 WORKING_DIR="$(dirname "$CURRENT_DIR")"
 CLASSPATH="$WORKING_DIR/lib/*:$WORKING_DIR/bin"
-OUTPUT_DIR="$W
+OUTPUT_DIR="$WORKING_DIR/bin"
+SRC_DIR="$WORKING_DIR/src"
+
+# get javac
+if [ -z "$JAVACCMD" ] ; then
+  if [ -n "$JAVA_HOME"  ] ; then
+    JAVACCMD="$JAVA_HOME/bin/javac"
+  else
+    JAVACCMD="$(command -v javac)"
+  fi
+fi
+
+# create bin folder
+if [ ! -d "$OUTPUT_D
