720e20e7467682b7fb30cc40fa031767d886761e
huappe <155170805+huappe@users.noreply.github.com>
2022-06-04 09:10:25 +0000

    debug info

--- scripts/simplify.sh
+++ scripts/simplify.sh
@@ -7,4 +7,12 @@ CLASSPATH="$WORKING_DIR/lib/*:$WORKING_DIR/bin"
 # get java
 if [ -z "$JAVACMD" ] ; then
   if [ -n "$JAVA_HOME"  ] ; then
-    JAVACMD="$JAVA_HOME/bin/java
+    JAVACMD="$JAVA_HOME/bin/java"
+  else
+    JAVACMD="$(command -v java)"
+  fi
+fi
+
+set -- "$@"
+$JAVACMD -Xmx1024m -classpath "$CLASSPATH" main.Console -json $1 $2
+python scripts/generate_sentences.py $2 $3
