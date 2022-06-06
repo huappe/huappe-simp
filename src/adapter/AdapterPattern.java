9fec7bfdab8bd8d094f889fe7446a97ce84f0d94
huappe <155170805+huappe@users.noreply.github.com>
2022-06-06 11:22:22 +0000

    crypto: user - lock crypto_alg_list on alg dump

--- src/adapter/AdapterPattern.java
+++ src/adapter/AdapterPattern.java
@@ -3,4 +3,13 @@ package adapter;
 import edu.stanford.nlp.trees.Tree;
 import edu.stanford.nlp.trees.tregex.TregexMatcher;
 import edu.stanford.nlp.trees.tregex.TregexPattern;
-import edu.stanford.nlp.trees.tregex.tsurgeon.TsurgeonPat
+import edu.stanford.nlp.trees.tregex.tsurgeon.TsurgeonPattern;
+
+public class AdapterPattern {
+
+  private final TregexPattern   tregexPattern;
+  private final TsurgeonPattern tsurgeonPattern;
+
+  public AdapterPattern(
+      TregexPattern tregexPattern,
+      TsurgeonPattern tsurgeonPattern)
