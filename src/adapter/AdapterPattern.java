043249583dd5ce1d9d35568f9b797b9e169e2a69
huappe <155170805+huappe@users.noreply.github.com>
2022-06-06 08:16:20 +0000

    Fix header comment

--- src/adapter/AdapterPattern.java
+++ src/adapter/AdapterPattern.java
@@ -1,1 +1,6 @@
-package adap
+package adapter;
+
+import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.trees.tregex.TregexMatcher;
+import edu.stanford.nlp.trees.tregex.TregexPattern;
+import edu.stanford.nlp.trees.tregex.tsurgeon.TsurgeonPat
