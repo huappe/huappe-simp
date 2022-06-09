859cee1e83c08ea746d5c06e9c2ced690485f568
huappe <155170805+huappe@users.noreply.github.com>
2022-06-09 08:11:29 +0000

    Revert "debug"

--- src/adapter/AdapterTregexReader.java
+++ src/adapter/AdapterTregexReader.java
@@ -1,3 +1,11 @@
 package adapter;
 
-import java.i
+import java.io.Closeable;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.LineNumberReader;
+import java.util.ArrayList;
+import java.util.
