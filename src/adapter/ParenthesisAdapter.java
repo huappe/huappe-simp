f78daa671b1be61f1b28a94452ca50f010fd0d04
huappe <155170805+huappe@users.noreply.github.com>
2022-07-02 16:31:34 +0000

    tracing/syscalls: Fix perf syscall tracing when syscall_nr == -1

--- src/adapter/ParenthesisAdapter.java
+++ src/adapter/ParenthesisAdapter.java
@@ -19,3 +19,88 @@ public class ParenthesisAdapter extends ISimpAnnotator {
 
   @Override
   protected void annotate(Tree tree) {
+    boolean found = true;
+    while (found) {
+      found = false;
+      TregexMatcher m = pattern1.matcher(tree);
+      if (m.find()) {
+        Tree np = new LabeledScoredTreeFactory().newLeaf("NP");
+        // np.setValue("NP");
+        Tree lrb = m.getNode("lrb");
+        Tree rrb = m.getNode("rrb");
+        Tree parent = lrb.parent(tree);
+        replace(parent, lrb, rrb, np);
+        found = true;
+      }
+      m = pattern2.matcher(tree);
+      if (m.find()) {
+        Tree np = new LabeledScoredTreeFactory().newLeaf("NP");
+        // np.setValue("NP");
+        Tree prn = m.getNode("prn");
+        Tree parent = prn.parent(tree);
+        Tree first = parent.firstChild();
+        replace2(parent, first, prn, np);
+        found = true;
+      }
+    }
+  }
+
+  // put [start, end) to newtree
+  private void replace2(Tree parent, Tree start, Tree end, Tree newTree) {
+    int indexOfStart = -1;
+    int indexOfEnd = -1;
+    for (int i = 0; i < parent.children().length; i++) {
+      if (parent.children()[i] == start) {
+        indexOfStart = i;
+      }
+      if (parent.children()[i] == end) {
+        indexOfEnd = i;
+        break;
+      }
+    }
+    if (indexOfStart == -1) {
+      throw new RuntimeException("cannot find start: " + parent);
+    }
+    if (indexOfEnd == -1) {
+      throw new RuntimeException("cannot find end: " + parent);
+    }
+    // replace
+    while (indexOfStart != indexOfEnd) {
+      newTree.addChild(parent.children()[indexOfStart]);
+      parent.removeChild(indexOfStart);
+      indexOfEnd--;
+    }
+    parent.addChild(indexOfStart, newTree);
+    return;
+  }
+
+  // put (start, end) to newtree
+  private void replace(Tree parent, Tree start, Tree end, Tree newTree) {
+    int indexOfStart = -1;
+    int indexOfEnd = -1;
+    for (int i = 0; i < parent.children().length; i++) {
+      if (parent.children()[i] == start) {
+        indexOfStart = i;
+      }
+      if (parent.children()[i] == end) {
+        indexOfEnd = i;
+        break;
+      }
+    }
+    if (indexOfStart == -1) {
+      throw new RuntimeException("cannot find start: " + parent);
+    }
+    if (indexOfEnd == -1) {
+      throw new RuntimeException("cannot find end: " + parent);
+    }
+    // replace
+    while (indexOfStart + 1 != indexOfEnd) {
+      newTree.addChild(parent.children()[indexOfStart + 1]);
+      parent.removeChild(indexOfStart + 1);
+      indexOfEnd--;
+    }
+    parent.addChild(indexOfStart + 1, newTree);
+    return;
+  }
+
+}
---
+++ src/annotation/ISimpAnnotations.java
@@ -0,0 +1,7 @@
+package annotation;
+
+import java.util.List;
+
+import edu.stanford.nlp.ling.CoreAnnotation;
+import edu.stanford.nlp.trees.Tree;
+import edu.stan
