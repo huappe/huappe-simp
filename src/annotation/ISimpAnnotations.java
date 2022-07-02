920210895132553919e32933d72c1fe5e35567af
huappe <155170805+huappe@users.noreply.github.com>
2022-07-02 21:24:08 +0000

    Use strict

--- src/annotation/ISimpAnnotations.java
+++ src/annotation/ISimpAnnotations.java
@@ -4,4 +4,13 @@ import java.util.List;
 
 import edu.stanford.nlp.ling.CoreAnnotation;
 import edu.stanford.nlp.trees.Tree;
-import edu.stan
+import edu.stanford.nlp.util.ErasureUtils;
+import extractor.SimplificationConstruct;
+
+public class ISimpAnnotations {
+
+  public static class ISimpAnnotation implements
+      CoreAnnotation<List<SimplificationConstruct>> {
+
+    @Override
+    
