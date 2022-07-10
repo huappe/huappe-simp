dea9dee0fb49953a1608961d1f39e423893f1381
huappe <155170805+huappe@users.noreply.github.com>
2022-07-10 19:16:17 +0000

    add state

--- src/annotation/ISimpAnnotations.java
+++ src/annotation/ISimpAnnotations.java
@@ -56,4 +56,18 @@ public class ISimpAnnotations {
   }
 
   public static class ParenthesisRefAnnotation implements
-      SimplificationA
+      SimplificationAnnotation {
+
+    @Override
+    public Class<Integer> getType() {
+      return Integer.class;
+    }
+
+    @Override
+    public String name() {
+      return "referred noun phrase";
+    }
+
+    @Override
+    public String simpleName() {
+   
