752d7e9131929f65a5ccac137e6a064fa4622bf4
huappe <155170805+huappe@users.noreply.github.com>
2022-09-04 20:49:17 +0000

    chore(project): sync generated files [skip ci]

--- src/annotator/HypernymyAnnotator.java
+++ src/annotator/HypernymyAnnotator.java
@@ -6,4 +6,11 @@ import annotation.ISimpAnnotations.HypernymyAnnotation;
 import annotation.ISimpAnnotations.HypernymyHypernymAnnotation;
 import annotation.ISimpAnnotations.HypernymyHyponymAnnotation;
 import detect.DetectionPattern;
-import det
+import detect.DetectionTregexReader;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.trees.tregex.TregexMatcher;
+
+public class HypernymyAnnotator extends ISimpAnnotator {
+
+  @Overri
