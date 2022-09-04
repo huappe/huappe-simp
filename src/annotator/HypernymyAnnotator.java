5964454c5daf0294f76ce76007c9a8c3bbacb697
huappe <155170805+huappe@users.noreply.github.com>
2022-09-04 16:51:14 +0000

    Whitespace removal

--- src/annotator/HypernymyAnnotator.java
+++ src/annotator/HypernymyAnnotator.java
@@ -1,1 +1,9 @@
-package 
+package annotator;
+
+import java.util.List;
+
+import annotation.ISimpAnnotations.HypernymyAnnotation;
+import annotation.ISimpAnnotations.HypernymyHypernymAnnotation;
+import annotation.ISimpAnnotations.HypernymyHyponymAnnotation;
+import detect.DetectionPattern;
+import det
