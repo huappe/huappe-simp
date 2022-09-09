c89df830276061836b1c5776a3e92c44519cfbc2
huappe <155170805+huappe@users.noreply.github.com>
2022-09-09 15:47:32 +0000

    Add some stuff

--- src/annotator/ISimpAnnotator.java
+++ src/annotator/ISimpAnnotator.java
@@ -1,1 +1,9 @@
-package an
+package annotator;
+
+import java.util.Collections;
+import java.util.Set;
+
+import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
+import edu.stanford.nlp.pipeline.Annotation;
+import
