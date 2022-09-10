1bab7dd7178d17651914b3b92cc00659fd96e5a1
huappe <155170805+huappe@users.noreply.github.com>
2022-09-10 08:46:59 +0000

    Integration

--- src/annotator/ISimpAnnotator.java
+++ src/annotator/ISimpAnnotator.java
@@ -6,4 +6,9 @@ import java.util.Set;
 import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
 import edu.stanford.nlp.pipeline.Annotation;
-import
+import edu.stanford.nlp.pipeline.Annotator;
+import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.trees.TreeCoreAnnotations.TreeAnnotation;
+import edu.stanford.nlp.util.CoreMap;
+
+public abstract class ISimpAnnotator implement
