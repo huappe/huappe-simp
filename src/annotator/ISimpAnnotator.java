b02bd9553fc537999237c9a579eeb54154ca14bf
huappe <155170805+huappe@users.noreply.github.com>
2022-09-11 11:36:27 +0000

    Upgrade KF5 version.

--- src/annotator/ISimpAnnotator.java
+++ src/annotator/ISimpAnnotator.java
@@ -11,4 +11,11 @@ import edu.stanford.nlp.trees.Tree;
 import edu.stanford.nlp.trees.TreeCoreAnnotations.TreeAnnotation;
 import edu.stanford.nlp.util.CoreMap;
 
-public abstract class ISimpAnnotator implement
+public abstract class ISimpAnnotator implements Annotator {
+
+  public static final Requirement ISMP_TREE = new Requirement(
+                                                "isimp tree annotation");
+
+  @Override
+  public Set<Requirement> requirementsSatisfied() {
+ 
