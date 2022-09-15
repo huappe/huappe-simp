7991f173a215cb808f26b1eb0d6ccb680939d67d
huappe <155170805+huappe@users.noreply.github.com>
2022-09-15 15:01:34 +0000

    reflection

--- src/annotator/ParenthesisAnnotator.java
+++ src/annotator/ParenthesisAnnotator.java
@@ -2,4 +2,11 @@ package annotator;
 
 import annotation.ISimpAnnotations.ParenthesisAnnotation;
 import annotation.ISimpAnnotations.ParenthesisElemAnnotation;
-import annotation.ISi
+import annotation.ISimpAnnotations.ParenthesisRefAnnotation;
+import detect.DetectionPattern;
+import detect.DetectionTregexReader;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.trees.tregex.TregexMatcher;
+
+public class Parenthesis
