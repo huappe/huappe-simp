2cb2b28cb95bd54675f99762d6e1f8f12b7a4c51
huappe <155170805+huappe@users.noreply.github.com>
2022-09-21 11:27:49 +0000

    add svg

--- src/annotator/RelativeClauseAnnotator.java
+++ src/annotator/RelativeClauseAnnotator.java
@@ -1,1 +1,7 @@
 package annotator;
+
+import java.util.List;
+
+import annotation.ISimpAnnotations.FullRelativeClauseAnnotation;
+import annotation.ISimpAnnotations.RedRelativeClauseAnnotation;
+import annotation.ISimpAnnotations.Relat
