f411c793cb20e629d35825e9db8dd46a19bf333d
huappe <155170805+huappe@users.noreply.github.com>
2022-09-28 18:35:08 +0000

    Update NAMESPACE

--- src/annotator/coordination/Coordination.java
+++ src/annotator/coordination/Coordination.java
@@ -1,1 +1,11 @@
-package annotator.coo
+package annotator.coordination;
+
+import java.util.LinkedList;
+
+import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.trees.tregex.TregexPattern;
+import edu.stanford.nlp.util.Pair;
+
+public abstract class Coordination {
+
+  protected static final Tr
