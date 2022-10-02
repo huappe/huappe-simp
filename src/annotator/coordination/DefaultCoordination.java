71691434ac48bb8215a702b1d059c549366140b4
huappe <155170805+huappe@users.noreply.github.com>
2022-10-02 13:16:28 +0000

    write test

--- src/annotator/coordination/DefaultCoordination.java
+++ src/annotator/coordination/DefaultCoordination.java
@@ -1,4 +1,10 @@
 package annotator.coordination;
 
 import edu.stanford.nlp.trees.Tree;
-import edu.s
+import edu.stanford.nlp.trees.tregex.TregexMatcher;
+import edu.stanford.nlp.util.Pair;
+
+public class DefaultCoordination extends Coordination {
+
+  public DefaultCoordination(Tree root, Tree coordination) {
+    super(root, coordinatio
