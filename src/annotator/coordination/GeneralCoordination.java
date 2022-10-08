05a65493f0c2f402f32f52736b4734c12c61f9b9
huappe <155170805+huappe@users.noreply.github.com>
2022-10-08 07:07:45 +0000

    ++

--- src/annotator/coordination/GeneralCoordination.java
+++ src/annotator/coordination/GeneralCoordination.java
@@ -2,4 +2,14 @@ package annotator.coordination;
 
 import edu.stanford.nlp.trees.Tree;
 import edu.stanford.nlp.trees.tregex.TregexMatcher;
-impor
+import edu.stanford.nlp.util.Pair;
+
+public class GeneralCoordination extends Coordination {
+
+  public GeneralCoordination(Tree root, Tree coordination) {
+    super(root, coordination);
+  }
+
+  @Override
+  public boolean isCoordination() {
+    TregexMatcher m = conjPattern.match
