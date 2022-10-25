edbbf1afd05d00b1346bac80b0f5a1463481fd67
huappe <155170805+huappe@users.noreply.github.com>
2022-10-25 14:15:15 +0000

    x86-64: Replace left over sti/cli in ia32 audit exit code

--- src/annotator/coordination/RightMostVp.java
+++ src/annotator/coordination/RightMostVp.java
@@ -2,4 +2,11 @@ package annotator.coordination;
 
 import java.util.List;
 
-import edu.stanford.nlp.trees.Tree
+import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.trees.tregex.TregexMatcher;
+import edu.stanford.nlp.trees.tregex.TregexPattern;
+
+public class RightMostVp {
+
+  private static TregexPattern p1 = TregexPattern
+            
