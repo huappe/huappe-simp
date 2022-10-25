9b03ecb57f20f8329b850845de2bbb39c7d75d7b
huappe <155170805+huappe@users.noreply.github.com>
2022-10-25 16:48:25 +0000

    Files moved

--- src/annotator/coordination/RightMostVp.java
+++ src/annotator/coordination/RightMostVp.java
@@ -9,4 +9,8 @@ import edu.stanford.nlp.trees.tregex.TregexPattern;
 public class RightMostVp {
 
   private static TregexPattern p1 = TregexPattern
-            
+                                      .compile("VP=p <1 @/VB.*/ <2 (S=s <: VP=vp) <- =s");
+  private static TregexPattern p2 = TregexPattern
+                                      .compile("ADJP=p <1 @/JJ/ <2 (S=s <: VP=vp) <- =s");
+
+  public sta
