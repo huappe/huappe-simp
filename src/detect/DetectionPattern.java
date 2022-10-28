61915c9b2ee531da5f071bd3a68a6888aa3dddb5
huappe <155170805+huappe@users.noreply.github.com>
2022-10-28 06:19:25 +0000

    usb: gadget: Fix g_ether interface link status

--- src/detect/DetectionPattern.java
+++ src/detect/DetectionPattern.java
@@ -1,3 +1,14 @@
 package detect;
 
-import edu.stanford.nlp.tr
+import edu.stanford.nlp.trees.tregex.TregexPattern;
+
+public class DetectionPattern {
+
+  private final TregexPattern   tregexPattern;
+
+  public DetectionPattern(
+      TregexPattern tregexPattern) {
+    this.tregexPattern = tregexPattern;
+  }
+
+  public final Treg
