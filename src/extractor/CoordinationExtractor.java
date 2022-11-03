416f5aa1a327c97e228cda0d35616156e4b13086
huappe <155170805+huappe@users.noreply.github.com>
2022-11-03 11:41:00 +0000

    fixed the test

--- src/extractor/CoordinationExtractor.java
+++ src/extractor/CoordinationExtractor.java
@@ -1,3 +1,9 @@
 package extractor;
 
 import org.apache.commons.lang3.Range;
+
+import utils.PtbUtils;
+import annotation.ISimpAnnotations.AdjpCoordinationAnnotation;
+import annotation.ISimpAnnotations.AdvpCoordinationAnnotation;
+import annotation.ISimpAnnotations.CoordinationAnnotation;
+import ann
