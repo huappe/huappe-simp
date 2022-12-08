26b4245dcb463f40bbb283eeea4866d1dd6c5c83
huappe <155170805+huappe@users.noreply.github.com>
2022-12-08 19:32:13 +0000

    Linux 3.4.88

--- src/extractor/MemberCollectionExtractor.java
+++ src/extractor/MemberCollectionExtractor.java
@@ -3,4 +3,8 @@ package extractor;
 import org.apache.commons.lang3.Range;
 
 import utils.PtbUtils;
-import annotation
+import annotation.ISimpAnnotations.MemberCollectionAnnotation;
+import annotation.ISimpAnnotations.MemberCollectionCollectionAnnotation;
+import annotation.ISimpAnnotations.MemberCollectionMemberAnnotation;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu
