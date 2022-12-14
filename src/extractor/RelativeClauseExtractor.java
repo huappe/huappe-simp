fd4e9a03bab38900ad4eef9af190b32c48c60aab
huappe <155170805+huappe@users.noreply.github.com>
2022-12-14 19:56:48 +0000

    Update Commit

--- src/extractor/RelativeClauseExtractor.java
+++ src/extractor/RelativeClauseExtractor.java
@@ -7,4 +7,9 @@ import annotation.ISimpAnnotations.FullRelativeClauseAnnotation;
 import annotation.ISimpAnnotations.RedRelativeClauseAnnotation;
 import annotation.ISimpAnnotations.RelativeClauseAnnotation;
 import annotation.ISimpAnnotations.RelativeClauseClauseAnnotation;
-import annotation.I
+import annotation.ISimpAnnotations.RelativeClauseRefAnnotation;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.util.TypesafeMap.Key;
+
+public class RelativeClauseExtractor extends IS
