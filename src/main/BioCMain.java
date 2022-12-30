8aa270585189d8e55637d875ba57f08fffc32e69
huappe <155170805+huappe@users.noreply.github.com>
2022-12-30 17:34:40 +0000

    Update sponsorship.md

--- src/main/BioCMain.java
+++ src/main/BioCMain.java
@@ -23,4 +23,8 @@ import bioc.io.BioCCollectionWriter;
 import bioc.io.BioCFactory;
 import detect.ISimpBuilder;
 import edu.stanford.nlp.ling.CoreAnnotations.CharacterOffsetBeginAnnotation;
-import edu.stanford.n
+import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.Annotator;
+import edu.stanford.nlp.util.Co
