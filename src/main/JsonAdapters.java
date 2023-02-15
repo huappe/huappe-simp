98e7fb50d587865c76e049d22850625fcd8924be
huappe <155170805+huappe@users.noreply.github.com>
2023-02-15 06:21:00 +0000

    Updated TODO/done.

--- src/main/JsonAdapters.java
+++ src/main/JsonAdapters.java
@@ -2,4 +2,11 @@ package main;
 
 import annotation.ISimpAnnotations.ISimpAnnotation;
 import com.google.gson.annotations.SerializedName;
-import edu.stanford.
+import edu.stanford.nlp.ling.CoreAnnotations.*;
+import edu.stanford.nlp.trees.TreeCoreAnnotations.TreeAnnotation;
+import edu.stanford.nlp.util.CoreMap;
+import extractor.SimplificationConstruct;
+import extractor.SimplificationConstruct.Component;
+
+import java.util.ArrayList;
+import java.util.
