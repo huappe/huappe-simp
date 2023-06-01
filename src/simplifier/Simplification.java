e2d391757817f9987dd1567a30f123a92d497a06
huappe <155170805+huappe@users.noreply.github.com>
2023-06-01 22:57:17 +0000

    Reindent code

--- src/simplifier/Simplification.java
+++ src/simplifier/Simplification.java
@@ -2,4 +2,12 @@
 //
 // import java.util.HashSet;
 // import java.util.LinkedList;
-// import java.util.
+// import java.util.Queue;
+// import java.util.Set;
+//
+// import utils.PtbUtils;
+// import edu.stanford.nlp.trees.MemoryTreebank;
+// import edu.stanford.nlp.trees.Tree;
+// import edu.stanford.nlp.trees.Treebank;
+//
+// public class Simpli
