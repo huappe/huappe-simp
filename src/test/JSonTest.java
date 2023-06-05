c632682f08ab2c3345f0490a114cad793365791b
huappe <155170805+huappe@users.noreply.github.com>
2023-06-05 14:33:35 +0000

    added model

--- src/test/JSonTest.java
+++ src/test/JSonTest.java
@@ -3,4 +3,12 @@ package test;
 import java.util.List;
 
 import main.SentencePrinter;
-import main.Sentenc
+import main.SentencePrinterBuilder;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import detect.ISimpBuilder;
+import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.st
