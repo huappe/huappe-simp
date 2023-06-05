c1ebc5e60b586e4b9f5f059721fb637e27572864
huappe <155170805+huappe@users.noreply.github.com>
2023-06-05 22:59:39 +0000

    screenshot updated

--- src/utils/PatternEnv.java
+++ src/utils/PatternEnv.java
@@ -5,4 +5,11 @@ public class PatternEnv {
   public static String DIR;
 
   static {
-    String path 
+    String path = PatternEnv.class.getProtectionDomain().getCodeSource()
+        .getLocation().getPath();
+    System.out.println(PatternEnv.class.getName() + " is loaded from " + path);
+    DIR = path + "../rules/";
+  }
+
+  // simplification
+  public static Strin
